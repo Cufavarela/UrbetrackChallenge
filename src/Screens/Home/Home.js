cfa341e6de074d25eef7bb2f78bffd3d3509bcee
Facundo Varela <cufa.varela@gmail.com>
2020-09-28 16:33:01 -0300

    home structure and goDark

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -11,19 +11,19 @@ function Home () {
     return (
         <>
             <Nav />
-            <div className="margin-left">
-                <div className="section">
-                    <ul className="itemList">
-                    {mock.map(foto =>
-                            <li className="item" key={foto.id}>
-                                <Link to={"/" + foto.id}>
-                                    <h2 className="subtitle autor">{foto.author}</h2>
-                                    <img src={foto.download_url}></img>
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
-                </div>
+            <div className="gallery">
+                <ul className="itemList">
+                {mock.map(foto =>
+                        <li className="item" key={foto.id}>
+                            <Link to={"/" + foto.id}>
+                                <img src={foto.download_url}></img>
+                                <div className="authorContainer">
+                                    <h2 className="author">{foto.author}</h2>
+                                </div>
+                            </Link>
+                        </li>
+                    )}
+                </ul>
             </div>
         </>
     )
